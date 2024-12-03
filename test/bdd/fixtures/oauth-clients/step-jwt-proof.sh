5e38599780d1dc594d7a29ac059de3825d40cfbf
Giulio Ruggeri <giulio.ruggeri@posteitaliane.it>
2024-12-03 15:12:54 +0100

    fix(test): clean up test profiles
    Signed-off-by: Giulio Ruggeri <giulio.ruggeri@posteitaliane.it>

--- test/bdd/fixtures/oauth-clients/step-jwt-proof.sh
+++ test/bdd/fixtures/oauth-clients/step-jwt-proof.sh
@@ -2,13 +2,14 @@
 #
 #SPDX-License-Identifier: Apache-2.0
 
-if [ $# -eq 0 ]
+if [ $# -lt 2 ]
   then
-    echo "No arguments supplied"
+    echo "You must provide audience and nonce args"
     exit 1
 fi
 
 basepath=$(dirname -- $(readlink -fn -- "$0"))
-nonce=${1}
+aud=${1}
+nonce=${2}
 
-echo -n "{\"iss\": \"wallet-poste\", \"aud\": \"https://guiding-tightly-shepherd.ngrok-free.app/oidc/idp/acme_issuer/v1.0\", \"iat\": $(date +%s), \"nonce\": \"${nonce}\"}" | step crypto jws sign --key ${basepath}/jwk.priv.json --password-file ${basepath}/key.pwd --typ openid4vci-proof+jwt --kid "did:jwk:$(basenc --base64url -w 0 < ${basepath}/jwk.pub.json)#0"
+echo -n "{\"iss\": \"wallet-poste\", \"aud\": \"${aud}\", \"iat\": $(date +%s), \"nonce\": \"${nonce}\"}" | step crypto jws sign --key ${basepath}/jwk.priv.json --password-file ${basepath}/key.pwd --typ openid4vci-proof+jwt --kid "did:jwk:$(basenc --base64url -w 0 < ${basepath}/jwk.pub.json)#0"
